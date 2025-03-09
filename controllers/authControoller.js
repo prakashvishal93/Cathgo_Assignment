ad8b3f34ec87f98278d69679f464bd3d26186f49
Vishal Prakash <prakashvishal93@Vishals-MacBook-Air.local>
2025-03-10 02:16:16 +0530

    added backend in separate for hosting

---
+++ controllers/authControoller.js
@@ -0,0 +1,12 @@
+const login = (req, res) => {
+    res.json({ message: 'Login successful' });
+  };
+  
+  const register = (req, res) => {
+    res.json({ message: 'Registration successful' });
+  };
+  
+  module.exports = {
+    login,
+    register,
+  };
